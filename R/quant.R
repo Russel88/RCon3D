fb6514856abab301d129432b022d0d0d2ee1b56b
Jakob Russel <cmf483@ku.dk>
2022-09-07 13:19:58 +0200

    v1.2.6 - New version

--- R/quant.R
+++ R/quant.R
@@ -1,16 +1,17 @@
 #' Quantifies the pixels in images
 #'
-#' Quantifies the number of pixels for each channel in each layer (z-stack)
+#' Quantifies the number of pixels for each channel in any direction (default z-stack)
 #' @param imgs The paths of array files; i.e. output from \code{loadIMG} or \code{findIMG} functions.
 #' @param channels Character vector with name(s) of channels. Channel names should be in the names of the array files
 #' @param naming Optional. Add metadata to the output dataframe by looking through names of array files. Should be a list of character vectors, each list element will be added as a variable. Example: naming=list(Time=c("T0","T1","T2")). The function inserts a variable called Time, and then looks through the names of the array files and inserts characters mathcing either T0, T1 or T2
 #' @param cores Number of cores for parallel computing
+#' @param axis Axis to aggregate over (1: x, 2: y, 3:z). Default 3 (z-axis)
 #' @keywords array image quantify
 #' @return A dataframe with number of pixels for each layer
 #' @import foreach doSNOW
 #' @export
 
-quant <- function(imgs,channels,naming = NULL,cores = 1) {
+quant <- function(imgs,channels,naming = NULL,cores = 1,axis=3) {
   
   # Loop for each channel
   qua.fin <- list()
@@ -41,7 +42,7 @@ quant <- function(imgs,channels,naming = NULL,cores = 1) {
       ch_t <- readRDS(ch_files[k])
       
       # Count for each layer
-      q <- apply(ch_t, 3, sum)
+      q <- apply(ch_t, axis, sum)
       
       # Output
       temp <- cbind(sub(paste0("_Array.*"),"",sub(".*/", "", ch_files[k])),channels[z],q,1:length(q))
